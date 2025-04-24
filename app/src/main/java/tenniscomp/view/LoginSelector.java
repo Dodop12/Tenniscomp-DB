4c358743555e1b9bf2e1310a713f92bc23d0adea
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2025-04-24 11:46:22 +0200

    Add first window to select user type

---
+++ app/src/main/java/tenniscomp/view/LoginSelector.java
@@ -0,0 +1,43 @@
+package tenniscomp.view;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class LoginSelector extends JFrame {
+
+    private final JButton playerButton;
+    private final JButton refereeButton;
+
+    public LoginSelector() {
+        setTitle("TennisComp - Seleziona utente");
+        setSize(350, 200);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        final JLabel label = new JLabel("Effettua l'accesso");
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+
+        this.playerButton = new JButton("Giocatore");
+        this.refereeButton = new JButton("Giudice Arbitro");
+
+        final JPanel buttonPanel = new JPanel();
+        buttonPanel.add(playerButton);
+        buttonPanel.add(refereeButton);
+
+        setLayout(new BorderLayout());
+        add(label, BorderLayout.NORTH);
+        add(buttonPanel, BorderLayout.CENTER);
+
+        add(buttonPanel);
+    }
+
+    public void display() {
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+}
