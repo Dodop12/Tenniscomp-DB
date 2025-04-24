f18683d2aa873af9d0214c93f1a31ca7efb8a70d
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2025-04-24 18:02:01 +0200

    Add class for sql queries

---
+++ app/src/main/java/tenniscomp/data/Queries.java
@@ -0,0 +1,26 @@
+package tenniscomp.data;
+
+public final class Queries {
+
+    public static final String ADD_PLAYER =
+        """
+        INSERT INTO giocatore (nome, cognome, email, data_nascita, sesso, telefono, username, password_hash)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        """;
+    
+    public static final String CHECK_PLAYER_LOGIN =
+        """
+        SELECT *
+        FROM giocatore
+        WHERE username = ?
+        AND password_hash = ?
+        """;
+
+        public static final String CHECK_GA_LOGIN =
+        """
+        SELECT *
+        FROM giudice_arbitro
+        WHERE username = ?
+        AND password_hash = ?
+        """;
+}
