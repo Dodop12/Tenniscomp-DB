aea587f1647862d5037121bc431e410559e7969b
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2025-04-29 15:13:30 +0200

    Add query to populate db

---
+++ sql/populate-db.sql
@@ -0,0 +1,5 @@
+INSERT INTO giocatore (nome, cognome, email, data_nascita, sesso, telefono, username, password_hash)
+VALUES ('Gino', 'Pino', 'gpino@mail.com', '1990-01-01', 'M', '333333333', 'gpino', 'ciao');
+
+INSERT INTO giudice_arbitro (nome, cognome, email, data_nascita, sesso, telefono, username, password_hash)
+VALUES ('Mario', 'Rossi', 'mrossi@mail.com', '1975-05-15', 'M', '333444444', 'admin', 'admin');
