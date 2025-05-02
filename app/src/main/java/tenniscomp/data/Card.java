7feb42e400a9170b4aec50675951f0e90cf0a940
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2025-05-02 23:43:22 +0200

    Update Card POJO after db fix

--- app/src/main/java/tenniscomp/data/Card.java
+++ app/src/main/java/tenniscomp/data/Card.java
@@ -1,16 +1,16 @@
 package tenniscomp.data;
 
+import java.sql.Connection;
+
 public class Card {
     private final int cardId;
     private final String cardNumber;
     private final String expiryDate;
-    private final int playerId;
 
-    public Card(final int cardId, final String cardNumber, final String expiryDate, final int playerId) {
+    public Card(final int cardId, final String cardNumber, final String expiryDate) {
         this.cardId = cardId;
         this.cardNumber = cardNumber;
         this.expiryDate = expiryDate;
-        this.playerId = playerId;
     }
 
     public int getCardId() {
@@ -25,11 +25,24 @@ public class Card {
         return expiryDate;
     }
 
-    public int getPlayerId() {
-        return playerId;
-    }
-
     public final class DAO {
-
+        
+        public static Card getCardById(final Connection connection, final int cardId) {
+            try (
+                var statement = DAOUtils.prepare(connection, Queries.GET_CARD_BY_ID, cardId);
+                var resultSet = statement.executeQuery();
+            ) {
+                if (resultSet.next()) {
+                    return new Card(
+                        resultSet.getInt("id_tessera"),
+                        resultSet.getString("numero"),
+                        resultSet.getString("scadenza")
+                    );
+                }
+                return null;
+            } catch (final Exception e) {
+                throw new DAOException(e);
+            }
+        }
     }
 }
