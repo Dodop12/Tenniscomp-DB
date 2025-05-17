4d7ec6a0d8d47e06c685c8d931d7784d11585c32
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2025-05-17 15:31:36 +0200

    Add league tie (team match) POJO class

---
+++ app/src/main/java/tenniscomp/data/LeagueTie.java
@@ -0,0 +1,73 @@
+package tenniscomp.data;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeagueTie {
+    private final int tieId;
+    private final String date;
+    private final String result;
+    private final int leagueId;
+    private final int homeTeamId;
+    private final int awayTeamId;
+
+    public LeagueTie(final int matchupId, final String date, final String result,
+            final int leagueId, final int homeTeamId, final int awayTeamId) {
+        this.tieId = matchupId;
+        this.date = date;
+        this.result = result;
+        this.leagueId = leagueId;
+        this.homeTeamId = homeTeamId;
+        this.awayTeamId = awayTeamId;
+    }
+
+    public int getTieId() {
+        return tieId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public int getLeagueId() {
+        return leagueId;
+    }
+
+    public int getHomeTeamId() {
+        return homeTeamId;
+    }
+
+    public int getAwayTeamId() {
+        return awayTeamId;
+    }
+
+    public final class DAO {
+
+        public static List<LeagueTie> getTiesByLeague(final Connection connection, final int leagueId) {
+            try (
+                var statement = DAOUtils.prepare(connection, Queries.GET_LEAGUE_TIES, leagueId);
+                var resultSet = statement.executeQuery();
+            ) {
+                final var ties = new ArrayList<LeagueTie>();
+                while (resultSet.next()) {
+                    ties.add(new LeagueTie(
+                        resultSet.getInt("id_incontro"),
+                        resultSet.getString("data"),
+                        resultSet.getString("risultato"),
+                        resultSet.getInt("id_campionato"),
+                        resultSet.getInt("id_squadra_casa"),
+                        resultSet.getInt("id_squadra_ospite")
+                    ));
+                }
+                return ties;
+            } catch (final Exception e) {
+                throw new DAOException(e);
+            }
+        }
+    }
+}
