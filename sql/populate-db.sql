cc1ba313e3a41c685c061c2bc3affc3444cb7636
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2025-05-17 16:04:19 +0200

    Add tournament and league sample

--- sql/populate-db.sql
+++ sql/populate-db.sql
@@ -8,3 +8,11 @@ INSERT INTO circolo (nome, indirizzo, citta)
 VALUES ('Circolo Tennis Roma', 'Via Bologna 1', 'Roma'),
        ('Circolo Tennis Milano', 'Via Rimini 24', 'Milano'),
        ('Circolo Tennis Napoli', 'Via Vesuviana 51', 'Napoli');
+
+INSERT INTO torneo (nome, data_inizio, data_fine, scadenza_iscrizioni, tipo,
+        sesso, limite_classifica, montepremi, id_ga, id_circolo)
+VALUES ('Torneo Open Milano', '2025-07-01', '2023-07-20', '2025-06-28',
+        'Singolare', 'M', '4.1', 1000.00, 1, 2);
+
+INSERT INTO campionato (serie, categoria, sesso, anno, id_ga)
+VALUES ('C', 'Open', 'F', 2025, 1);
