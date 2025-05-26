c6c3d25c2721bce637eee0ca4f6e01503cf13dea
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2025-05-26 11:21:52 +0200

    Add method to retrieve a date range given a league category

--- app/src/main/java/tenniscomp/utils/LeagueCategory.java
+++ app/src/main/java/tenniscomp/utils/LeagueCategory.java
@@ -1,5 +1,6 @@
 package tenniscomp.utils;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,5 +11,41 @@ public enum LeagueCategory {
         return Arrays.stream(values())
                 .map(LeagueCategory::toString)
                 .toList();
+    }
+
+    public Pair<LocalDate, LocalDate> getBirthDateRange() {
+        final var now = LocalDate.now();
+        final int currentYear = now.getYear();
+        final var minDate = LocalDate.of(1900, 1, 1);
+        final int categoryAge = this == OPEN ? 0 : Integer.parseInt(this.name().substring(1));
+        
+        return switch (this) {
+            case OPEN -> new Pair<>(minDate, now);
+            
+            case U10 -> new Pair<>(LocalDate.of(currentYear - 10, 1, 1), now);
+            case U12 -> createUnderCategoryRange(currentYear, categoryAge);
+            case U14 -> createUnderCategoryRange(currentYear, categoryAge);
+            case U16 -> createUnderCategoryRange(currentYear, categoryAge);
+            
+            case O45 -> createOverCategoryRange(currentYear, categoryAge, 50);
+            case O50 -> createOverCategoryRange(currentYear, categoryAge, 55);
+            case O55 -> createOverCategoryRange(currentYear, categoryAge, 60);
+            case O60 -> new Pair<>(minDate, LocalDate.of(currentYear - 60, 12, 31));
+            default -> throw new IllegalArgumentException("Categoria non valida: " + this);
+        };
+    }
+    
+    private Pair<LocalDate, LocalDate> createUnderCategoryRange(final int currentYear, final int maxAge) {
+        return new Pair<>(
+            LocalDate.of(currentYear - maxAge, 1, 1),
+            LocalDate.of(currentYear - (maxAge - 1), 12, 31)
+        );
+    }
+    
+    private Pair<LocalDate, LocalDate> createOverCategoryRange(final int currentYear, final int minAge, final int maxAge) {
+        return new Pair<>(
+            LocalDate.of(currentYear - maxAge, 1, 1),
+            LocalDate.of(currentYear - minAge, 12, 31)
+        );
     }
 }
