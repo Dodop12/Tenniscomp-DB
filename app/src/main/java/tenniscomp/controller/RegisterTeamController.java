426c9e196195be86da42d2e8ae8d7166919fbc70
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2025-05-29 09:07:31 +0200

    Add abstract controller class for a generic table containing players

--- app/src/main/java/tenniscomp/controller/RegisterTeamController.java
+++ app/src/main/java/tenniscomp/controller/RegisterTeamController.java
@@ -5,102 +5,67 @@ import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
-import javax.swing.JOptionPane;
-import javax.swing.RowFilter;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
-
-import tenniscomp.data.Club;
 import tenniscomp.data.Player;
 import tenniscomp.model.Model;
 import tenniscomp.utils.TableUtils;
 import tenniscomp.view.RegisterTeamWindow;
 
-public class RegisterTeamController {
+public class RegisterTeamController extends BasePlayerTableController {
     
-    private final RegisterTeamWindow view;
     private final List<Player> eligiblePlayers;
     private final Set<Integer> selectedRows;
-    private final Model model;
     private final int leagueId;
-
     private Runnable onTeamRegisteredCallback;
 
     public RegisterTeamController(final RegisterTeamWindow view, final List<Player> eligiblePlayers, 
             final Model model, final int leagueId) {
-        this.view = view;
+        super(view, model);
         this.eligiblePlayers = eligiblePlayers;
         this.selectedRows = new HashSet<>();
-        this.model = model;
         this.leagueId = leagueId;
         
-        loadEligiblePlayers();
-        setupSearchFunctionality();
+        loadPlayers();
+    }
+    
+    @Override
+    protected List<Player> getPlayersToDisplay() {
+        return eligiblePlayers;
+    }
+    
+    @Override
+    protected void setupAdditionalComponents() {
         setupTableSelection();
-        setupButtonListeners();
     }
     
-    private void loadEligiblePlayers() {
-        final var tableModel = view.getTableModel();
-        TableUtils.clearTable(tableModel);
-        
-        for (final var player : eligiblePlayers) {
-            final Club club = Optional.ofNullable(player.getClubId())
-                    .map(model::getClubById)
-                    .orElse(null);
-            final Object[] rowData = {
-                player.getPlayerId(),
-                player.getSurname(),
-                player.getName(),
-                player.getBirthDate(),
-                player.getGender().getCode(),
-                player.getEmail(),
-                player.getPhone(),
-                player.getRanking().getLabel(),
-                club != null ? club.getName() : ""
-            };
-            tableModel.addRow(rowData);
-        }
-        
-        TableUtils.adjustColumnWidths(view.getPlayersTable());
+    @Override
+    protected void setupButtonListeners() {
+        final var registerView = (RegisterTeamWindow) view;
+        registerView.setConfirmButtonListener(e -> handleConfirm());
+        registerView.setCancelButtonListener(e -> view.dispose());
     }
-    
-    private void setupSearchFunctionality() {
-        view.setSearchFieldListener(new DocumentListener() {
-            @Override
-            public void insertUpdate(final DocumentEvent e) {
-                applySearchFilter();
-            }
 
-            @Override
-            public void removeUpdate(final DocumentEvent e) {
-                applySearchFilter();
-            }
-
-            @Override
-            public void changedUpdate(final DocumentEvent e) {
-                applySearchFilter();
-            }
-        });
+    public void setOnTeamRegisteredCallback(final Runnable callback) {
+        this.onTeamRegisteredCallback = callback;
     }
     
-    private void applySearchFilter() {
-        final String searchText = view.getSearchField().getText();
-        final var sorter = view.getTableSorter();
-        
-        if (searchText.trim().length() == 0) {
-            sorter.setRowFilter(null);
-        } else {
-            // Filter on the surname column (index 1)
-            sorter.setRowFilter(RowFilter.regexFilter("(?i)" + searchText, 1));
+    public List<Player> getSelectedPlayers() {
+        final var selectedPlayers = new ArrayList<Player>();
+        for (final int modelRow : this.selectedRows) {
+            final int playerId = getPlayerIdFromRow(modelRow);
+            final Player player = findPlayerById(playerId);
+            if (player != null) {
+                selectedPlayers.add(player);
+            }
         }
+        return selectedPlayers;
     }
     
     private void setupTableSelection() {
-        view.setPlayersTableMouseListener(new MouseAdapter() {
+        final var registerView = (RegisterTeamWindow) view;
+
+        registerView.setPlayersTableMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(final MouseEvent e) {
                 if (e.getClickCount() == 1) {
@@ -136,14 +101,8 @@ public class RegisterTeamController {
             }
         }
         
-        // Use TableUtils to highlight the selected rows
         TableUtils.highlightRows(table, viewRowsToHighlight);
         table.repaint();
-    }
-    
-    private void setupButtonListeners() {
-        view.setConfirmButtonListener(e -> handleConfirm());
-        view.setCancelButtonListener(e -> view.dispose());
     }
     
     private void handleConfirm() {
@@ -177,32 +136,5 @@ public class RegisterTeamController {
         } else {
             showError("Errore durante la registrazione della squadra.");
         }
-    }
-    
-    public void setOnTeamRegisteredCallback(final Runnable callback) {
-        this.onTeamRegisteredCallback = callback;
-    }
-    
-    public List<Player> getSelectedPlayers() {
-        final var selectedPlayers = new ArrayList<Player>();
-        for (final int modelRow : this.selectedRows) {
-            final int playerId = (int) view.getTableModel().getValueAt(modelRow, 0);
-            for (final var player : this.eligiblePlayers) {
-                if (player.getPlayerId() == playerId) {
-                    selectedPlayers.add(player);
-                    break; // Found the player, no need to continue searching
-                }
-            }
-        }
-        return selectedPlayers;
-    }
-    
-    private void showError(final String message) {
-        JOptionPane.showMessageDialog(
-            view,
-            message,
-            "Errore",
-            JOptionPane.ERROR_MESSAGE
-        );
     }
 }
