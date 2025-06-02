8f1f1a9c748039de9fe861aadb6f7de32932e070
Edoardo Pierfederici <edoardo.pierfederici@studio.unibo.it>
2025-06-02 17:11:15 +0200

    Remove unused files

--- app/src/main/java/tenniscomp/App.java
+++ app/src/main/java/tenniscomp/App.java
@@ -2,13 +2,13 @@ package tenniscomp;
 
 import java.sql.SQLException;
 
-import tenniscomp.controller.MainControllerImpl;
+import tenniscomp.controller.MainController;
 import tenniscomp.data.DAOUtils;
 
 public class App {
 
     public static void main(final String[] args) throws SQLException {
         final var connection = DAOUtils.localMySQLConnection("tenniscomp", "root", "");
-        new MainControllerImpl(connection).start();
+        new MainController(connection).start();
     }
 }
--- app/src/main/java/tenniscomp/controller/MainController.java
+++ app/src/main/java/tenniscomp/controller/MainController.java
@@ -1,6 +1,31 @@
 package tenniscomp.controller;
 
-public interface MainController {
+import java.sql.Connection;
 
-    void start();
+import tenniscomp.model.ModelImpl;
+import tenniscomp.view.LoginSelector;
+import tenniscomp.view.LoginWindow;
+
+public class MainController {
+
+    private final Connection connection;
+
+    public MainController(final Connection connection) {
+        this.connection = connection;
+    }
+
+    public void start() {
+        final var loginSelector = new LoginSelector();
+        loginSelector.setPlayerListener(e -> initializeLogin(false, loginSelector));
+        loginSelector.setRefereeListener(e -> initializeLogin(true, loginSelector));
+        loginSelector.display();
+    }
+
+    private void initializeLogin(final boolean isReferee, final LoginSelector loginSelector) {
+        loginSelector.dispose();
+        final var model = new ModelImpl(connection);
+        final var loginView = new LoginWindow(isReferee);
+        new LoginController(loginView, model);
+    }
+    
 }
